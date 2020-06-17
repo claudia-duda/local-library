2fdc08db49fd7860821e311c5f3cefd90cd561ff
claudia-duda <claudiaeduarda_duda@hotmail.com>
2020-06-17 13:32:34 -0300

    field validation

--- pessoas/views.py
+++ pessoas/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 def register(request):
@@ -11,24 +12,39 @@ def register(request):
         password = request.POST['password']
         password2 = request.POST['password2']
 
-        '''if campo_vazio(nome):
-            messages.error(request , 'O campo nome não pode ficar em branco')
-            return redirect('cadastro')
-        if campo_vazio(email):
-            messages.error(request , 'O campo email não pode ficar em branco')
-            return redirect('cadastro')
-        if senhas_nao_sao_iguais(senha,senha2):
-            messages.error(request , 'As senhas não são iguais')
-            return redirect('cadastro')
-        if User.objects.filter(email=email).exists():
-            messages.error(request , 'Usuário já cadastrado')
-            return redirect('cadastro') 
-        if User.objects.filter(username=nome).exists():
-            messages.error(request , 'Usuário já cadastrado')
-            return redirect('cadastro')'''
+        if not first_name.strip():
+            messages.error(request , 'Field fisrt name invalid')
+            return redirect('register')
+
+        if not last_name.strip():
+            messages.error(request , 'Field last name invalid')
+            return redirect('register')
+
+        if not username.strip():
+            messages.error(request , 'Field username invalid')
+            return redirect('register')  
+
+        if not email.strip():
+            messages.error(request , 'Field email invalid')
+            return redirect('register')
+
+        if not password == password2:
+            messages.error(request , 'the password is different')
+            return redirect('register')
+
+        if User.objects.filter(email= email).exists():
+            messages.error(request , 'User exists')
+            return redirect('register') 
+
+        if User.objects.filter(username= username ).exists():
+            messages.error(request , 'User exists')
+            return redirect('register')
+            
         user = User.objects.create_user(username= username, email= email, password= password, first_name= first_name)
         user.save()
-        '''messages.success(request, 'User registed')'''
+        messages.success(request, 'User registed')
+
         return redirect('login')
+
     else:
         return render(request,'registration/sign_in.html')  
